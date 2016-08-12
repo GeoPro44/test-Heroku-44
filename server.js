5f00aa591e086e6a349a3dbc737190c15eb3bc0f
George Prokopik <George.Prokopik@aesynt.com>
2016-08-12 16:16:45 -0400

    3

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(bodyParser.json());
 // Initialize the app.
 var server = app.listen(process.env.PORT || 3000, function () {
 	var port = server.address().port;
-	console.log("App now running on port " + port);
-	
+	console.log("App now running on port", port);
+
 	app.get('/', function(req, res) {
 	  res.sendFile("src/index.html");
 	});
